2adaabb0158d0b62639768eaf169ec9d4cec1981
Sahno86 <mishenkinav@gmail.com>
2025-01-19 16:39:01 +0500

    test_vscode_synhrone

--- Infohit/main_menu/test_main_menu_logo_click.py
+++ Infohit/main_menu/test_main_menu_logo_click.py
@@ -21,3 +21,5 @@ logo = driver.find_element("xpath", "//a[contains(@class, 'header__nav__logo-nam
 wait.until(EC.element_to_be_clickable(logo))
 logo.click()
 assert driver.title == "Агрегатор онлайн-курсов ИнфоХит — маркетплейс обучающих дистанционных программ", 'Заголовок неверен'
+
+print('1 тест выпонен')
